bddc1d06d084af6dddb9a9ece0f49d075c7d9173
IzzyEhnes <izzyehnes@gmail.com>
2020-10-14 15:43:56 -0700

    File: Add Main.java

---
+++ Main.java
@@ -0,0 +1,32 @@
+class AminoAcid
+{
+    private String aminoAcid;
+
+
+
+    public AminoAcid()
+    {
+
+    }
+
+
+
+    public AminoAcid(String inString)
+    {
+        aminoAcid = inString;
+    }
+
+}
+
+
+
+
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+	// write your code here
+    }
+}
