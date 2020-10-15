201d93a84f3a81a3cceebc9c8175550135f00912
IzzyEhnes <izzyehnes@gmail.com>
2020-10-15 13:24:19 -0700

    Feature: Add findPotentialMatches and isMatch methods

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+
+
+
 class AminoAcid
 {
     private char aminoAcid;
@@ -15,7 +19,6 @@ class AminoAcid
     {
         aminoAcid = inChar;
     }
-
 }
 
 
@@ -39,6 +42,69 @@ class Peptide
     public Peptide(String inString)
     {
         peptide = inString;
+    }
+
+
+
+    public void findPotentialMatches()
+    {
+        String protein = "YMKATWVDDAGHJIECQARNDW";
+
+        String inPeptide = this.peptide;
+
+        int peptideLength = peptide.length();
+        int proteinLength = protein.length();
+
+        String proteinSubstring = "";
+
+        boolean match = false;
+        for (int i = 0; i < protein.length() && match == false; i++)
+        {
+            if (protein.charAt(i) == inPeptide.charAt(0))
+            {
+                int endIndex = i + peptideLength;
+
+                if (endIndex > protein.length())
+                {
+                    endIndex = proteinLength;
+                }
+
+                proteinSubstring = protein.substring(i, endIndex);
+
+                System.out.println("Protein substring");
+                System.out.println(proteinSubstring);
+
+                match = isMatch(peptide, proteinSubstring);
+            }
+        }
+
+        if (match)
+        {
+            System.out.print(String.format("Peptide %s was found within protein %s", peptide, protein));
+        }
+    }
+
+
+
+    public boolean isMatch(String peptide, String proteinSubstring)
+    {
+        if (peptide.equals(proteinSubstring))
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+    @Override
+    public String toString()
+    {
+        return peptide;
     }
 }
 
@@ -75,6 +141,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter a peptide: ");
+        Peptide targetPeptide = new Peptide(scanner.nextLine());
+
+        System.out.println(targetPeptide);
+
+        targetPeptide.findPotentialMatches();
     }
 }
